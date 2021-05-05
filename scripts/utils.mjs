4eb5a4fc5812e70cef5c1f776f7ae6ada2389ab8
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-05-05 10:42:03 +0800

    Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -2,7 +2,7 @@ import { DEFAULTS } from './commons.mjs';
 
 export function makeFromParameter() {
     const { duration } = DEFAULTS;
-    const now = dayjs();
+    const now = dayjs().utc();
     const from = now.subtract(duration, 'minute');
     return [from.format('YYYY-MM-DDTHH:mm:ss'), from, now];
 }
