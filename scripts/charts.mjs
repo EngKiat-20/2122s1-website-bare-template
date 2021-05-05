64df10fdc8effbcc22389daede1a4f1007e99f49
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-05-05 10:50:50 +0800

    Update charts.mjs

--- scripts/charts.mjs
+++ scripts/charts.mjs
@@ -1,3 +1,8 @@
+import { createColumnChart, drawChart } from './google_chart.mjs';
+import { getErrorChartPayload } from './connector.mjs';
+import { makeFromParameter } from './utils.mjs';
+import { DEFAULTS } from './commons.mjs';
+
 function getBucketKey(value, bucketSize) {
     return value - (value % bucketSize);
 }
@@ -31,4 +36,29 @@ function makeErrorChartPayloadToTimestampBuckets(payload, fromDayJs, toDayJs) {
         minValue,
         maxValue,
     );
+}
+
+const charts = [
+    // error chart
+    {
+        options: {},
+        containerId: 'error-rate',
+        create: createColumnChart,
+        getData: getErrorChartPayload,
+        convertToGraphData: makeErrorChartPayloadToTimestampBuckets,
+        draw: (chart, array, options) => drawChart(chart, array, options),
+    },
+];
+
+export function initialize() {
+    charts.forEach(({ options, containerId, create, getData, convertToGraphData, draw }) => {
+        const chartContainerDom = document.getElementById(containerId);
+        const chartDom = chartContainerDom.querySelector('.chart');
+        const chart = create(chartDom);
+        const [from, fromDayJs, toDayJs] = makeFromParameter();
+        const { duration } = DEFAULTS;
+        getData(from, duration)
+            .then((data) => convertToGraphData(data, fromDayJs, toDayJs))
+            .then((array) => draw(chart, array, options));
+    });
 }
